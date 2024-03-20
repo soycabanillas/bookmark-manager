14542be121fb0c2e552c2b9cf71b97fda40ec3fd
Alejandro Cabanillas <soycabanillas@gmail.com>
2024-03-19 19:55:37 -0700

    electron and vite scaffold

--- BookmarkManager/appmanager.js
+++
@@ -1,5 +0,0 @@
-import { initialize as gridManager } from "./gridmanager.js";
-import { initialize as popupelement } from "./popupelement.js";
-
-gridManager("infinite-grid")
-popupelement("popup-element")
